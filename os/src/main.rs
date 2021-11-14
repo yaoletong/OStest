cd33480c6f5766e89b8c9dc2922f5578b51e9be8
yaoletong <944704744@qq.com>
2021-11-14 19:32:59 +0800

    实验七

---
+++ os/src/main.rs
@@ -0,0 +1,57 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+#![feature(global_asm)]
+#![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
+
+#[macro_use]
+extern crate bitflags;
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+mod syscall;
+mod trap;
+mod loader;
+mod config;
+mod task;
+mod timer;
+mod sync;
+mod mm;
+
+global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            sbss as usize as *mut u8,
+            ebss as usize - sbss as usize,
+        ).fill(0);
+    }
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("[kernel] Hello, world!");
+    mm::init();
+    println!("[kernel] back to world!");
+    mm::remap_test();
+    task::add_initproc();
+    println!("after initproc!");
+    trap::init();
+    trap::enable_timer_interrupt();
+    timer::set_next_trigger();
+    loader::list_apps();
+    task::run_tasks();
+    panic!("Unreachable in rust_main!");
+}
